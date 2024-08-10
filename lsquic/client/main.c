aba5801aaefba459a7e091b28736ce296b32806d
HikaruTashiro <hikarutashiro@hotmail.com>
2024-08-09 23:56:13 -0300

    feat: start of example implementation for client

---
+++ lsquic/client/main.c
@@ -0,0 +1,19 @@
+#include <lsquic.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void logger(const char *tag, const char *message) {
+  time_t now;
+  time(&now);
+  printf("%s [%s]: %s\n", ctime(&now), tag, message);
+}
+
+int main(int argc, char *argv[]) {
+  if (lsquic_global_init(LSQUIC_GLOBAL_CLIENT) != 0) {
+    logger("ERROR", "unable to initialize lsquic");
+    exit(EXIT_FAILURE);
+  }
+
+  lsquic_global_cleanup();
+  return EXIT_SUCCESS;
+}
